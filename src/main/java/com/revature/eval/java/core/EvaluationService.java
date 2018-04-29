f7d0d85ea644376640d1cc9400e8b8234ac9391e
ianH92 <ianhdrummond1992@gmail.com>
2018-04-28 22:54:14 -0700

    Completed question 16

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -675,8 +675,19 @@ public class EvaluationService {
 	 * @return
 	 */
 	public boolean isPangram(String string) {
-		// TODO Write an implementation for this method declaration
-		return false;
+		int lettersTracker = 0;
+		
+		for(int i = 0; i < string.length(); i++) {
+			char curr = string.charAt(i);
+			
+			if(curr >= 'a' && curr <= 'z') {
+				lettersTracker |= (1 << (curr - 'a'));
+			} else if(curr >= 'A' && curr <= 'Z') {
+				lettersTracker |= (1 << (curr - 'A'));
+			}
+		}
+		
+		return(lettersTracker == 67108863) ? true : false;
 	}
 
 	/**
